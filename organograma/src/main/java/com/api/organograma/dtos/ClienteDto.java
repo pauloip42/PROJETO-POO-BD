c527e1692810e1a2a71d27142b05956f54b46251
Amanda Silva Guimaraes <amanda.silva@gec.inatel.br>
2022-05-26 17:51:30 -0300

    atributos cliente

--- organograma/src/main/java/com/api/organograma/dtos/ClienteDto.java
+++ organograma/src/main/java/com/api/organograma/dtos/ClienteDto.java
@@ -1,4 +1,53 @@
 package com.api.organograma.dtos;
 
 public class ClienteDto {
+
+    private String nomeEmpresa;
+    private String responsavel;
+    private int cpnj;
+    private int telefone;
+    private String email;
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getNomeEmpresa() {
+        return nomeEmpresa;
+    }
+
+    public void setNomeEmpresa(String nomeEmpresa) {
+        this.nomeEmpresa = nomeEmpresa;
+    }
+
+    public int getCpnj() {
+        return cpnj;
+    }
+
+    public void setCpnj(int cpnj) {
+        this.cpnj = cpnj;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
 }

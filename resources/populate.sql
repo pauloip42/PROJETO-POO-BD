6177e215a04b825a75d2fb38f8dc62fb0b1ee05a
Paulo Lopes <phlj.junior@gmail.com>
2022-06-15 13:48:34 -0300

    :ok_hand: update sql script

--- resources/populate.sql
+++ resources/populate.sql
@@ -1,4 +1,6 @@
-INSERT INTO nucleo VALUES(1, 'Software', 'Nucleo de Desenvolvimento', 'ND');
+USE organograma;
+
+-- INSERT INTO nucleo VALUES(1, 'Software', 'Nucleo de Desenvolvimento', 'ND');
 INSERT INTO nucleo VALUES(2, 'Gestão', 'Nucleo Assuntos Internos', 'NAI');
 INSERT INTO nucleo VALUES(3, 'Comercial', 'Nucleo Comercial', 'NC');
 INSERT INTO nucleo VALUES(4, 'Produção', 'Nucleo de Produção', 'NP');
@@ -6,6 +8,7 @@ INSERT INTO nucleo VALUES(4, 'Produção', 'Nucleo de Produção', 'NP');
 INSERT INTO cliente VALUES(100, '12345678912341', 'cliente1@email.com', 'empresa1', 'responsavel1', '123456789121');
 INSERT INTO cliente VALUES(101, '12345678912342', 'cliente2@email.com', 'empresa2', 'responsavel2', '223456789122');
 INSERT INTO cliente VALUES(102, '12345678912343', 'cliente3@email.com', 'empresa3', 'responsavel3', '223456789123');
+INSERT INTO cliente VALUES(103, '12345678912344', 'cliente4@email.com', 'empresa4', 'responsavel4', '223456789124');
 
 INSERT INTO membro VALUES(100, 'Acessor', 'GEC', 'paulo@email.inatel.br', 'Paulo', 1);
 INSERT INTO membro VALUES(101, 'Presidente', 'GEC', 'amanda@email.inatel.br', 'Amanda', 3);
@@ -29,7 +32,7 @@ INSERT INTO projeto_membro VALUES(102, 100);
 INSERT INTO projeto_membro VALUES(102, 102);
 INSERT INTO projeto_membro VALUES(102, 104);
 
--- select * from projeto;
+select * from projeto;
 
 -- select * from membro;
 
